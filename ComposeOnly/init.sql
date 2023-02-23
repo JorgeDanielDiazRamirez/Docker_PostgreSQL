6c6015f00fe8c6ed8d2e94816b6ad99b621a178d
Jorge Daniel Diaz Ramirez <84112604+JorgeDanielDiazRamirez@users.noreply.github.com>
2023-02-22 23:27:23 -0500

    Update init.sql

--- ComposeOnly/init.sql
+++ ComposeOnly/init.sql
@@ -1,1 +1,14 @@
+CREATE TABLE employees (
+    emp_no      INT             NOT NULL,
+    birth_date  DATE            NOT NULL,
+    first_name  VARCHAR(14)     NOT NULL,
+    last_name   VARCHAR(16)     NOT NULL,
+    gender      VARCHAR(1),
+    hire_date   DATE            NOT NULL,
+    PRIMARY KEY (emp_no)
+);
 
+COPY employees
+FROM [filepath for emp.csv]
+DELIMITER ','
+CSV HEADER;
